3dbd9db51a73207375430d68f4d241c8dd3800dc
yusufstawan <yusufsetiyawan45@gmail.com>
2022-03-27 09:39:36 +0700

    update host

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ const NotesValidator = require('./validator/notes');
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
